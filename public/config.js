696effe3af3381c72900c00a1720c3aeeae3de41
lhliang <a-lhliang@qq.com>
2023-05-08 13:35:35 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,15 +1,12 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: '网站状态监测',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur2124406-7e6f02b53b7ba4957a82fd90',
   ],
 
   // 日志天数
@@ -21,16 +18,8 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
-    },
-    {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
-    },
-    {
       text: 'Blog',
-      url: 'https://abo.xyz/'
+      url: 'https://lhliang.com/'
     },
   ],
 };
